9a8de71c6303d179de0df0d1ca0b8d2a3da333e3
Julio Cesar <julioreis044@gmail.com>
2023-08-22 23:55:48 -0300

    Criando tela de Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,13 +1,30 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/tailwind.css')
-</head>
-<body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-</body>
-</html>
+<link rel="stylesheet" href="/css/app.css">
+
+@section('title', 'Drinks')
+
+@section('content')
+
+<div class="login">
+
+  <form action="">
+    <div class="title-login">
+      <h2>Fazer login</h2>
+    </div>
+
+    <div>
+      <label for="email">E-mail</label>
+      <input type="email" id="email" name="email" placeholder="Digite seu e-mail...">
+    </div>
+
+    <div>
+      <label for="password">Senha</label>
+      <input type="password" id="email" name="email" placeholder="Digite sua senha...">
+    </div>
+
+    <div class="divButton">
+      <button type="submit" id="buttonEntrar">Entrar</button>
+      <button id="buttonCadastrar">Cadastrar</button>
+    </div>
+  </form>
+   
+</div>
