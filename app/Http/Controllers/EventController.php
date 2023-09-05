9499c68b981511318a9d9f09d31e57808a488330
Julio Cesar <julioreis044@gmail.com>
2023-09-05 08:50:57 -0300

    Finalizando rotas no controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,11 +6,55 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    public function index(){
+    public function cerveja(){
         return view('cerveja');
     }
 
     public function coquetel(){
-        
+        return view('coquetel');
+    }
+
+    public function diverso(){
+        return view('diverso');
+    }
+
+    public function drinks(){
+        return view('drinks');
+    }
+
+    public function energetico(){
+        return view('energetico');
+    }
+
+    public function petisco(){
+        return view('petisco');
+    }
+
+    public function refrigerante(){
+        return view('refrigerante');
+    }
+
+    public function sobremesa(){
+        return view('sobremesa');
+    }
+
+    public function suco(){
+        return view('suco');
+    }
+
+    public function sushi(){
+        return view('sushi');
+    }
+
+    public function vinho(){
+        return view('vinho');
+    }
+
+    public function login(){
+        return view('login');
+    }
+
+    public function admin(){
+        return view('admin');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,50 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\EventController;
  
-Route::get('/', [EventController::class, 'index']);
-
-Route::get('/coquetel', function () {
-    return view('coquetel'); 
-});
-
-Route::get('/diverso', function () {
-    return view('diverso'); 
-});
-
-Route::get('/drinks', function () {
-    return view('drinks'); 
-});
-
-Route::get('/energetico', function () {
-    return view('energetico'); 
-});
-
-Route::get('/petisco', function(){return view('petisco');});
-
-Route::get('/refrigerante', function () {
-    return view('refrigerante'); 
-});
-
-Route::get('/sobremesa', function () {
-    return view('sobremesa'); 
-});
-
-Route::get('/suco', function () {
-    return view('suco'); 
-});
-
-Route::get('/sushi', function () {
-    return view('sushi'); 
-});
-
-Route::get('/vinho', function () {
-    return view('vinho'); 
-}); 
-
-Route::get('/login', function () {
-    return view('login'); 
-});
-
-Route::get('/admin', function () {
-    return view('admin'); 
-});
+Route::get('/', [EventController::class, 'cerveja']);
+Route::get('/coquetel', [EventController::class, 'coquetel']);
+Route::get('/diverso', [EventController::class, 'diverso']);
+Route::get('/drinks', [EventController::class, 'drinks']);
+Route::get('/energetico', [EventController::class, 'energetico']);
+Route::get('/petisco', [EventController::class, 'petisco']);
+Route::get('/refrigerante', [EventController::class, 'refrigerante']);
+Route::get('/sobremesa', [EventController::class, 'sobremesa']);
+Route::get('/suco', [EventController::class, 'suco']);
+Route::get('/sushi', [EventController::class, 'sushi']);
+Route::get('/vinho', [EventController::class, 'vinho']);
+Route::get('/login', [EventController::class, 'login']);
+Route::get('/admin', [EventController::class, 'admin']);
