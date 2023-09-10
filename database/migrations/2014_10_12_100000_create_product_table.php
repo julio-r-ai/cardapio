c448da944e00409d225a1922a37077e550495bca
Julio Cesar <julioreis044@gmail.com>
2023-09-10 11:26:10 -0300

    Configurando migrate

--- database/migrations/2014_10_12_100000_create_product_table.php
+++ database/migrations/2014_10_12_100000_create_product_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('product', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('img');
+            $table->string('description');
+            $table->string('price');
+            $table->string('category'); 
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('product');
     }
 };
